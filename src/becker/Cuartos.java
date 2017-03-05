2e80c72fcc180768403ee8f1dbe75f08f2604eb7
JAVIER <JAVIER@JAVIER-PC>
2017-03-05 17:38:13 -0500

    Yaaaaa!!!!

--- src/becker/Cuartos.java
+++ src/becker/Cuartos.java
@@ -10,149 +10,100 @@ import becker.robots.*;
  * @author Estudiante
  */
 public class Cuartos {
-    private City gotham;
-    private Thing seg;
-    private Robot bats;
+     private City Gotham;
+    private Robot Bats;
+    private Thing[] seg;
+    private Wall[] Paredes;
+    private Dibujar Num1;
+    private Dibujar Num2;
+    private Dibujar Num3;
     
-public Cuartos(){
-    this.gotham = new City (11, 11);
-    this.bats = new Robot(gotham,6,2,Direction.NORTH);    
-    for (int i=1; i <0; i++){
-        for (int y = 1; y < 0; y++){
-    this.seg = new Thing (this.gotham,y, i);
-    }
-    }
-    for (int w= 1; w < 6; w++){
-    Wall one = new Wall (gotham, w, 1, Direction.WEST );
-    }
-    for (int e=1;e < 6; e++){
-        Wall two = new Wall (gotham, e, 9, Direction.EAST);
-    }
-    for (int n=1; n< 10; n++){
-        Wall three = new Wall(gotham, 1, n, Direction.NORTH);
-    }
-    //copiado y modificado del codigo de @leotalorac
-     Wall p1 = new Wall(gotham,5,1,Direction.SOUTH);   
-     Wall p2 = new Wall(gotham,5,3,Direction.SOUTH);   
-     Wall p3 = new Wall(gotham,5,4,Direction.SOUTH);   
-     Wall p4 = new Wall(gotham,5,6,Direction.SOUTH);   
-     Wall p5 = new Wall(gotham,5,7,Direction.SOUTH);   
-     Wall p6 = new Wall(gotham,5,9,Direction.SOUTH);
-     //fin de la copia.
-    for (int i1=1; i1 < 6; i1++){
-        Wall five = new Wall(gotham, i1, 4, Direction.WEST);
-    }
-    for (int i2=1; i2 < 6; i2++){
-        Wall six = new Wall(gotham, i2, 7, Direction.WEST);
-    }
-}
-public void movimiento (){
-    boolean a,b,c,d,e,f,g = false;
-    // inicia.
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    for (int i = 0; i < 2; i++){
-        bats.turnLeft();
-    }
-    //Reconocer movimientos.
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            d = true;
-            } else {
-        d = false;
-    }
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            c = true;
-            } else {
-        c = false;
-    }
-    bats.move();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            b = true;
-            } else {
-        b = false;
-    }
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            a = true;
-            } else {
-        a = false;
-    }
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            f = true;
-            } else {
-        f = false;
-    }
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            g = true;
-            } else {
-        g = false;
-    }
-    for(int i = 0; i < 2; i++){
-        bats.turnLeft();
-    }
-    bats.move();
-    bats.turnLeft();
-    bats.move();
-    if (this.bats.canPickThing()){
-            bats.pickThing();
-            bats.putThing();
-            e = true;
-            } else {
-        e = false;
-    }
-    bats.move();    
-    bats.turnLeft();
-    bats.move();
-    for (int i = 0; i < 3; i++){
-    bats.turnLeft();
-    }
-    bats.move();
-    if (d = true){
-        if (c = true){
-            if (b = true ){
-                if (a = true ){
-                    if(f = true){
-                        if (g = true){
-                            if ( e = true){
-                                System.out.println("8");
-                            }
-                        }
-                    }
+    public Cuartos(int n1, int n2, int n3){
+        Gotham = new City(10, 12);
+        Bats = new Robot(Gotham, 6, 2, Direction.NORTH);
+        Num1 = new Dibujar(n1, 1);
+        Num2 = new Dibujar(n2, 2);
+        Num3 = new Dibujar(n3, 3);
+        seg = new Thing[39];
+        Paredes = new Wall[35];
+        int x = 0;
+        for(int i=1;i<6;i++){
+            for(int j=1;j<10;j++){
+                if(j<4)
+                    if(Num1.NumberWrite(i, j))
+                        continue;
+                if(j>=4&&j<7)
+                    if(Num2.NumberWrite(i, j))
+                        continue;
+                if(j>=7)
+                    if(Num3.NumberWrite(i, j))
+                        continue;
+                seg[x]=new Thing(Gotham, i, j);
+                x++;
+            }
+        }
+        int y = 0;
+        for(int i=1;i<6;i++){
+            for(int j=1;j<10;j++){
+                if(i==1){
+                    Paredes[y]=new Wall(Gotham, i, j, Direction.NORTH);
+                    y++;
+                }
+                if(j==1||j==4||j==7){
+                    Paredes[y]=new Wall(Gotham, i, j, Direction.WEST);
+                    y++;
+                }
+                if(i==5&&j!=2&&j!=5&&j!=8){
+                    Paredes[y]=new Wall(Gotham, i, j, Direction.SOUTH);
+                    y++;
+                }
+                if(j==9){
+                    Paredes[y]=new Wall(Gotham, i, j, Direction.EAST);
+                    y++;
                 }
             }
         }
     }
-   bats.turnLeft();
-   bats.move();
-   bats.move();
-   bats.move();
-   bats.turnLeft();
-}
-
+    
+    public void In(){
+        for(int i=0;i<3;i++)
+            Bats.move();
+        Bats.turnLeft();
+    }
+    
+    public boolean[] movimiento(){
+        boolean[] x = new boolean[7];
+        x[6]=Bats.canPickThing();
+        Bats.move();
+        for(int i=0;i<3;i++)
+            Bats.turnLeft();
+        Bats.move();
+        x[5]=Bats.canPickThing();
+        Bats.turnLeft();
+        Bats.turnLeft();
+        Bats.move();
+        Bats.move();
+        for(int i=4;i>0;i--){
+            x[i]=Bats.canPickThing();
+            Bats.move();
+            if(i!=2)
+                Bats.turnLeft();
+            Bats.move();
+        }
+        x[0]=Bats.canPickThing();
+        return x;
+    }
+    
+    public void getOut(){
+        Bats.turnLeft();
+        for(int i=0;i<5;i++)
+            Bats.move();
+    }
+    
+    public void Next(){
+        Bats.turnLeft();
+        for(int i=0;i<3;i++)
+            Bats.move();
+        Bats.turnLeft();
+    }
 }
